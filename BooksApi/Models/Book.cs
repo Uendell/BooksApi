4bc1eb77ba9f9e0451ea33af06596b8c83509d9f
Uendell Rios <riosuendell@outloock.com>
2024-04-15 23:12:55 -0300

    Add project files.

---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        internal object?[]? id;
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        public string? Title { get; set; }
+        [Required(ErrorMessage = "Author is required")]
+        public string? Author { get; set; }
+        public string? Genre { get; set; }
+        public string? Language { get; set; }
+        public string? Publisher { get; set; }
+        [Required(ErrorMessage = "Release date is required")]
+        public DateTime? ReleaseDate { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Number of pages must be bigger than 0")]
+        public int? NumberOfPages {  get; set; }
+        [Required(ErrorMessage = "Price is required")]
+        [Range(5, double.MaxValue, ErrorMessage = "Price must be bigger than or equal 5")]
+        public decimal? Price { get; set; }
+
+
+    }
+}
---
+++ BooksApi/Models/BookContext.cs
@@ -0,0 +1,9 @@
+﻿using BooksApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApi.Models;
+
+public class BookContext(DbContextOptions<BookContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books { get; set; } = null!;
+}
